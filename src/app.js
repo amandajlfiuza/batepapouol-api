03f7528f376725563258dcdd2f7a53c7ec841869
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-09-05 10:20:46 -0300

    post participants

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient, ObjectId } from 'mongodb';
-import dotenv from 'dotenv';
-dotenv.config();
-
-export default function App () {
-    
-}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
 import express from 'express';
 import cors from 'cors';
-import getMessages from './routes/getMessages.js';
-import getParticipants from './routes/getParticipants.js';
-import postMessages from './routes/postMessages.js';
-import postParticipants from './routes/postParticipants.js';
-import postStatus from './routes/postStatus.js';
+import { MongoClient } from 'mongodb';
+import joi from 'joi';
+import dotenv from 'dotenv';
+dotenv.config();
+import dayjs from 'dayjs';
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => db = mongoClient.db("batepapo-uol"));
 
-getMessages();
-getParticipants();
-postMessages();
-postParticipants();
-postStatus();
+const participantSchema = joi.object({
+    name: joi.string().empty(' ').required()
+});
+
+server.post('/participants', async (req, res) => {
+    const participant = req.body;
+    const validation = participantSchema.validate(participant);
+    if (validation.error) {
+        res.sendStatus(422);
+        return;
+    }
+    
+    const response = await db.collection('participants').find({ name: participant.name }).toArray();
+    if (response.length > 0) {
+        res.sendStatus(409);
+        return;
+    }
+
+    db.collection('participants').insertOne({
+        ...participant,
+        lastStatus: Date.now()
+    });
+
+    db.collection('messages').insertOne({
+        from: participant.name,
+        to: 'Todos',
+        text: 'entrou na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
+    });
+    res.sendStatus(201);
+});
 
 server.listen(5000, () => console.log('Listening on port 5000'));
